db8c8d78bb2c85afc76eca0e48420e4bb82a2314
Gabriel Viana <gabrielviana@MacBook-Pro-de-Gabriel.local>
2023-08-08 22:56:42 -0300

    alteração

--- src/router/app.routes.jsx
+++ src/router/app.routes.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+
+
 import { Home } from '../pages/Home';
 import { Details } from '../pages/Details';
 import { New } from '../pages/New';
@@ -7,18 +9,17 @@ import { Favorites } from '../pages/Favorites';
 import { MyOrders } from '../pages/MyOrder';
 import { useAuth } from '../hooks/auth';
 
+
 export function AppRoutes() {
   const { user } = useAuth();
-  const isAdmin = user && typeof user.isAdmin === 'function' ? user.isAdmin() : false;
-
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/details/:id' element={<Details/>}/>
-      <Route path='/favorites' element={<Favorites/>}/>
-      <Route path='/orders' element={<MyOrders/>}/>
-      {isAdmin && <Route path='/new' element={<New/>}/> }
-      {isAdmin && <Route path='/edit/:id' element={<Edit/>}/> }
+        <Route path='/' element={<Home/>}/>
+        <Route path='/details/:id' element={<Details/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+        <Route path='/orders' element={<MyOrders/>}/>
+        { user.isAdmin && <Route path='/new' element={<New/>}/> }
+        { user.isAdmin && <Route path='/edit/:id' element={<Edit/>}/> }
     </Routes>
   );
 }
