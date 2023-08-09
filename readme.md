0aa4fd75f972d1637a4d458e5b8ecde0f4e062ac
Gabriel Viana <gabrielviana@MacBook-Pro-de-Gabriel.local>
2023-08-08 23:47:35 -0300

    readme

---
+++ readme.md
@@ -0,0 +1,40 @@
+<h1>Food Explorer </h1>
+
+<img src='./src/assets/Capa.png'>
+
+<br/>
+
+<h3>Descrição do projeto</h3>
+
+<p>Projeto final do bootcamp explorer da rockeseat</p>
+
+<h3>Funcionalidades</h3>
+<ul>
+  <li>Cadastro de usuário</li>
+  <li>Cadastro de produto</li>
+  <li>Carrinho de compras</li>
+  <li>Edição de produto</li>
+
+</ul>
+
+<h3>🛠 Tecnologias</h3>
+<ul>
+  <li>React</li>
+  <li>Styled-Components</li>
+  <li>Phospor-React</li>
+  <li>Axios</li>
+
+</ul>
+
+<h3>Como usar</h3>
+<ul>
+  <li>Para usar este projeto você precisa clonar este repositório e clonar 
+  </li>
+
+  <li>Depois do projeto clonado é necessário dar um npm install para intalar as depêndencias e depois um npm run dev para rodar </li>
+
+</ul>
+
+<h3>Autor</h3>
+
+<p>Gabriel Viana</p>
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -7,19 +7,20 @@ import { Favorites } from '../pages/Favorites';
 import { MyOrders } from '../pages/MyOrder';
 import { New } from '../pages/New';
 
+import { useAuth } from '../hooks/auth';
 
 export function AppRoutes(){
+
+    const { user } = useAuth();
     return (
         <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/details' element={<Details/>}/>
-            <Route path='/edit' element={<Edit/>}/>
             <Route path='/favorite' element={<Favorites/>}/>
             <Route path='/orders' element={<MyOrders/>}/>
-            <Route path='/new' element={<New/>}/>
+            { user.isAdmin && <Route path='/new' element={<New/>}/> }
+            { user.isAdmin && <Route path='/edit/:id' element={<Edit/>}/> } 
         </Routes>
     )
 }
 
- {/* { user.isAdmin && <Route path='/new' element={<New/>}/> }
- { user.isAdmin && <Route path='/edit/:id' element={<Edit/>}/> } */}
